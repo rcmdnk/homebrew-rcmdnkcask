3cf8bbf28470776cee32664f6d24274ec63ec2d4
rcmdnk <rcmdnk@gmail.com>
2014-05-10 06:08:17 +0900

    added new casks

---
+++ Cask/madruby.rb
@@ -0,0 +1,7 @@
+class Madruby < Cask
+  url 'http://www.wingsforpigs.com/downloads/MadRuby_1.0.3.dmg'
+  homepage 'http://www.wingsforpigs.com/MadRuby/MadRuby.html'
+  version '1.0.3'
+  sha256 'a0d45b892b28e9fd4e5ae17578e3afc6e385307635c422d71292d47b177d7480'
+  link 'MadRuby.app'
+end
---
+++ Cask/simplefloatingclock.rb
@@ -0,0 +1,7 @@
+class Simplefloatingclock < Cask
+  url 'http://www.splook.com/Software/distrib/sfc.zip'
+  homepage 'http://www.splook.com/Software/Simple_Floating_Clock.html'
+  version '1.8'
+  sha256 'dbfe0b92aaf57b63962f1ded518fac01916a3f11d871d1cfed9c59ffb627bf2d'
+  link 'Simple Floating Clock/SimpleFloatingClock.app'
+end

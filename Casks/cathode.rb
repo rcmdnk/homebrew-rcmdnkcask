a1e2f776479a42548b60aed6f53c77d6b7de047c
rcmdnk <rcmdnk@gmail.com>
2014-05-10 06:11:53 +0900

    added casks

---
+++ Casks/cathode.rb
@@ -0,0 +1,7 @@
+class Cathode < Cask
+  url 'http://store.secretgeometry.com/download.php?id=7'
+  homepage 'http://www.secretgeometry.com/apps/cathode/'
+  version '2.1.2'
+  sha256 'bf34bfff4ff57fda5f5bf2ab633a9c9f364b517d62f8fdbdceb00c264037ad71'
+  link 'Cathode.app'
+end

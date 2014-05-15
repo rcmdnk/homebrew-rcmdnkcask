47b9515f46b0a7073a1f4c32146a6ded7d2afbf0
rcmdnk <rcmdnk@gmail.com>
2014-05-15 10:35:47 +0900

    added notify

---
+++ Casks/notify.rb
@@ -0,0 +1,7 @@
+class Notify < Cask
+  url 'http://www.macupdate.com/download/32217/notify-216.zip'
+  homepage 'http://www.macupdate.com/app/mac/32217/notify-pro'
+  version '2.1.6'
+  sha256 'cc11cedeeaedd15619dd6b89777fc48c9da4d6ad7fd9f9e541d258739a9a3f6f'
+  link 'Notify.app'
+end

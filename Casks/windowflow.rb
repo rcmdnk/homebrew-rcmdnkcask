18c9a3dfcab23d6cb676b08270370649465ccdb6
rcmdnk <rcmdnk@gmail.com>
2014-05-26 09:50:20 +0900

    add windowflow

---
+++ Casks/windowflow.rb
@@ -0,0 +1,7 @@
+class Notify < Cask
+  url 'http://files.windowflow.com/windowflow-standard.zip'
+  homepage 'http://most-advantageous.com/windowflow/'
+  version 'latest'
+  sha256 :no_check
+  link 'WindowFlow.app'
+end

f339ccc5194adad53cf153c670a69a674c56653f
rcmdnk <rcmdnk@gmail.com>
2014-07-07 18:14:41 +0900

    addded lightningkeyboard

---
+++ Casks/lightningkeyboard.rb
@@ -0,0 +1,7 @@
+class Lightningkeyboard < Cask
+  url 'http://morishin.me/file/LightningKeyboard.zip'
+  homepage 'http://morishin.hatenablog.com/entry/2014/07/05/213741'
+  version 'latest'
+  sha256 :no_check
+  link 'LightningKeyBoard/LightningKeyboard.app'
+end

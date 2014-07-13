6080864a5fe12bbeb6b2f4073433b968932f8a14
rcmdnk <rcmdnk@gmail.com>
2014-07-14 00:17:51 +0900

    added font-migu1m

---
+++ Casks/font-migu1m.rb
@@ -0,0 +1,7 @@
+class FontMig1m < Cask
+  url 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-1m-20130617.zip'
+  homepage 'http://mix-mplus-ipa.sourceforge.jp/migu/'
+  sha256 'c52025471b560e5aba73ec2327a160d6e514def05964056dbb1a8409e5870b45'
+  font 'migu-1m-20130617/migu-1m-bold.ttf'
+  font 'migu-1m-20130617/migu-1m-regular.ttf'
+end

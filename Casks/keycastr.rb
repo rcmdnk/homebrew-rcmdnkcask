168f5f74929e8c78fc417629a046b122286b4091
rcmdnk <rcmdnk@gmail.com>
2014-07-30 15:13:21 +0900

    added keycastr

---
+++ Casks/keycastr.rb
@@ -0,0 +1,7 @@
+class Keycastr < Cask
+  url 'http://dw.cbsi.com/redir?ttag=restart_download_click&ptid=3001&pagetype=product_pdl&asid=11008453&astid=2&edid=3&tag=link&siteid=4&destUrl=&onid=2075&oid=3001-2075_4-125977&rsid=cbsidownloadcomsite&sl=en&sc=us&pdguid=download%3A11008453&topicguid=biz-soft%2Fpresentation-tools&topicbrcrm=mac%20software&pid=11008453&mfgid=10035070&merid=10035070&ctype=dm&cval=NONE&devicetype=desktop&pguid=d8aa852c365ce06cdfbc724e&viewguid=PYjEbw5GdizzaHkdkQhCQ1ZBJbrmjffo4BO3&destUrl=http%3A%2F%2Fsoftware-files-a.cnet.com%2Fs%2Fsoftware%2F11%2F00%2F84%2F53%2Fkeycastr_0.8.0.dmg%3Ftoken%3D1406736276_3d8137255aaa9d2967301e8c13b03bea%26fileName%3Dkeycastr_0.8.0.dmg'
+  homepage 'https://github.com/sdeken/keycastr'
+  version '0.8.0'
+  sha256 'ecf59a2e46a8ca450c12f426a4d41000de1c3010a5c46fe12f23fdc647f3f1e7'
+  link 'KeyCastr.app'
+end

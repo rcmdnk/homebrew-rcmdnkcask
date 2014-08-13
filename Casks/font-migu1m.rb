c59ad954010e584f8c5c7452480acbbe1403febb
rcmdnk <rcmdnk@gmail.com>
2014-08-13 13:05:18 +0900

    Casks/font-migu1m.rb, from darwin13

--- Casks/font-migu1m.rb
+++ Casks/font-migu1m.rb
@@ -2,7 +2,7 @@ class FontMigu1m < Cask
   version '20130617'
   sha256 'c52025471b560e5aba73ec2327a160d6e514def05964056dbb1a8409e5870b45'
 
-  url 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-1m-#{version}.zip'
+  url "http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-1m-#{version}.zip"
   homepage 'http://mix-mplus-ipa.sourceforge.jp/migu/'
 
   font 'migu-1m-20130617/migu-1m-bold.ttf'

df952adf1c0d85a0c2ac9f6be68ebccee1057acb
rcmdnk <rcmdnk@gmail.com>
2015-05-07 15:05:03 +0900

    added configureapplicationdocktile.rb

--- Casks/notify.rb
+++ Casks/notify.rb
@@ -2,7 +2,7 @@ cask :v1 => 'notify' do
   version '2.1.6'
   sha256 'cc11cedeeaedd15619dd6b89777fc48c9da4d6ad7fd9f9e541d258739a9a3f6f'
 
-  url 'http://www.macupdate.com/download/32217/notify-216.zip'
+  url "http://www.macupdate.com/download/32217/notify-#{version.gsub('.','')}.zip"
   homepage 'http://www.macupdate.com/app/mac/32217/notify-pro'
   license :unknown
 

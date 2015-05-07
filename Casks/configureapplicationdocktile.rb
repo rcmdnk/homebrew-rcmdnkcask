0b94f21b8d787642f0967df1bdeb4d54034f6e85
rcmdnk <rcmdnk@gmail.com>
2015-05-07 15:07:55 +0900

    added configureapplicationdocktile.rb

---
+++ Casks/configureapplicationdocktile.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'configureapplicationdocktile' do
+  version '1.0'
+  sha256 '064f5c24ad7e26aa80c9c82b7c6049d96cd7396d53035fb4276fa638dc7d28c5'
+
+  url "http://boredzo.org/cadt/Configure_Application_Dock_Tile-#{version}.zip"
+  homepage 'http://boredzo.org/cadt/'
+  license :unknown
+  app 'Configure Application Dock Tile 1.0/Configure Application Dock Tile.app'
+
+end

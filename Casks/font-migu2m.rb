34605a74803808f0e8e53da8c8349a72c771de5c
rcmdnk <rcmdnk@gmail.com>
2015-06-01 17:46:23 +0900

    added font-migu2m

---
+++ Casks/font-migu2m.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-migu2m' do
+  version '20130617'
+  sha256 '25092541e927bbebc094f9c1d7cfaec3e8e2c7a997c360c507a3433ed11e8278'
+
+  url "http://osdn.jp/frs/redir.php?m=iij&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-2m-#{version}.zip"
+  homepage 'http://mix-mplus-ipa.sourceforge.jp/migu/'
+  license :unknown
+
+  font 'migu-1m-20130617/migu-2m-bold.ttf'
+  font 'migu-1m-20130617/migu-2m-regular.ttf'
+end

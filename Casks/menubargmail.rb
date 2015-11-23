49f05a905798ff53bf40eeb1666539b6a0833697
rcmdnk <rcmdnk@gmail.com>
2015-11-23 18:19:19 +0900

    updated menubarmail

--- Casks/menubargmail.rb
+++ Casks/menubargmail.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'menubargmail' do
   version '0.0.9'
-  sha256 'de820ea43b79e5dcaa2e2fe147c567e985365847d0323855640b278778eb42f4'
+  sha256 'ee9847526e15c8e87f33dc74d0e0eb7ac2adae69bf80c90474f90301aaf7257d'
 
   url "https://github.com/rcmdnk/MenuBarGmail/archive/v#{version}.tar.gz"
   name 'MenuBarGmail'

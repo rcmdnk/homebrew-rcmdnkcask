0f111950b55d947f32ec44e31d774f8c90c0e111
rcmdnk <rcmdnk@gmail.com>
2020-07-29 08:23:32 +0900

    update vem

--- Casks/vem.rb
+++ Casks/vem.rb
@@ -1,6 +1,6 @@
 cask 'vem' do
   version '0.1.0'
-  sha256 '5fc397c912d04f9ca56106cf12ef5b83f2de19d889e3bbb391b0b4ec34b5b757'
+  sha256 'fb4e7ef9507358bde599279bf95c2c99793af0f6c1d87036504dc2d7b36f3969'
 
   url "https://github.com/rcmdnk/VEM/releases/download/v#{version}/VEM-v#{version}.zip"
   name 'VEM'
